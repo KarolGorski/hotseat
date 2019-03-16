1369b8ce9185f0137d1b0f61e5a82c5416b00a37
Karol Górski <goralk2870@gmail.com>
2019-03-16 14:17:17 +0100

    init commit

---
+++ src/main/java/eu/gzs/hotseat/HotseatApplication.java
@@ -0,0 +1,13 @@
+package eu.gzs.hotseat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotseatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotseatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/gzs/hotseat/HotseatApplicationTests.java
@@ -0,0 +1,16 @@
+package eu.gzs.hotseat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HotseatApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
