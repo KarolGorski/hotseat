ca1b84d7e5d0ab5a672c7be865f07221cd336a32
Krzysiek <sen.krzysiek@gmail.com>
2019-03-18 22:03:16 +0100

    Dodano Hibernate + łączenie z lokalnym PostgreSQL (szczegóły w pliku konfiguracyjnym application.properties i hibernate.cfg.xml <- konfig hibernate'a)

--- src/main/java/eu/gzs/hotseat/HotseatApplication.java
+++ src/main/java/eu/gzs/hotseat/HotseatApplication.java
@@ -1,7 +1,12 @@
 package eu.gzs.hotseat;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
 
 @SpringBootApplication
 public class HotseatApplication {
@@ -10,6 +15,30 @@ public class HotseatApplication {
 		SpringApplication.run(HotseatApplication.class, args);
 		//moj testowy komentarz git
 		//moj lepszy testowy komentarz git
+
+		//Stworzenie i otwarcie sesji z bazą
+		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+
+		//Tworzenie Person - tu bez bean'ów narazie, testowo
+		Person person = new Person();
+		person.setName("Janusz"); person.setCountry("Poland");
+
+		//zapis obiektu do bazy z automatycznym mapowaniem do tabeli
+		session.save(person);
+		session.getTransaction().commit();
+
+		//odczyt z bazy z automatycznym mapowaniem do obiektu
+		List<Person> personList = session.createQuery("from Person").list();
+
+		for(Person p : personList){
+			System.out.println(p.toString());
+		}
+
+		//zamkniecie sesji
+		session.close();
+
 	}
 
 }
