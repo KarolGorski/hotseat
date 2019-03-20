d9f3f82685fb089e0c713285d60c1e8ab9dd8cd1
Krzysztof Seń <sen.krzysztof@gmail.com>
2019-03-20 17:43:40 +0100

    Uzupełniono encję Seat o brakujące pola i metody

--- src/main/java/eu/gzs/hotseat/model/Seat.java
+++ src/main/java/eu/gzs/hotseat/model/Seat.java
@@ -1,4 +1,33 @@
 package eu.gzs.hotseat.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="Seat")
 public class Seat {
+    @Id
+    @Column(name="Id")
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
+    private int client_id;
+
+    public Seat(int client_id) {
+        this.client_id = client_id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getClient_id() {
+        return client_id;
+    }
+
+    public void setClient_id(int client_id) {
+        this.client_id = client_id;
+    }
 }
