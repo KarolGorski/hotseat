3b2013e362ee7efed2ebe11892434574c1ee7cd4
Krzysztof Seń <sen.krzysztof@gmail.com>
2019-03-20 17:29:11 +0100

    Stworzono encję Client z id i name

--- src/main/java/eu/gzs/hotseat/model/Client.java
+++ src/main/java/eu/gzs/hotseat/model/Client.java
@@ -1,4 +1,14 @@
 package eu.gzs.hotseat.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="Client")
 public class Client {
+    @Id
+    @Column(name="Id")
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
 }
