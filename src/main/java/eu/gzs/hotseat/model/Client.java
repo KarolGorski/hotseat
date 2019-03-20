0931b91aba83f88d93b74baeac092e4a9d7fb34e
Krzysztof Seń <sen.krzysztof@gmail.com>
2019-03-20 17:30:39 +0100

    Dodano gettery i settery  oraz konstruktor do Client

--- src/main/java/eu/gzs/hotseat/model/Client.java
+++ src/main/java/eu/gzs/hotseat/model/Client.java
@@ -11,4 +11,24 @@ public class Client {
     private int id;
 
     private String name;
+
+    public Client(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
