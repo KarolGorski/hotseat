a7356b1f639b28b703c9bdc57c2fce14b0364ea5
Krzysztof Seń <sen.krzysztof@gmail.com>
2019-03-20 17:45:56 +0100

    Uzupełniono encję Movie o brakujące pola i metody

--- src/main/java/eu/gzs/hotseat/model/Movie.java
+++ src/main/java/eu/gzs/hotseat/model/Movie.java
@@ -1,4 +1,45 @@
 package eu.gzs.hotseat.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="Movie")
 public class Movie {
+    @Id
+    @Column(name="Id")
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    private int duration;
+
+    public Movie(String name, int duration) {
+        this.name = name;
+        this.duration = duration;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
 }
