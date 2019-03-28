9c41ce40f8c485cd89e718a02c9597294c1d8371
Krzysiek <sen.krzysiek@gmail.com>
2019-03-28 18:51:06 +0100

    Dodano SeanceController

---
+++ src/main/java/eu/gzs/hotseat/controller/SeanceController.java
@@ -0,0 +1,38 @@
+package eu.gzs.hotseat.controller;
+
+import eu.gzs.hotseat.daoclasses.SeanceService;
+import eu.gzs.hotseat.model.Seance;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping({"/seances"})
+public class SeanceController {
+    private SeanceService seanceService = new SeanceService();
+
+    @GetMapping
+    public List<Seance> getAllSeances() {
+        return seanceService.getAllSeances();
+    }
+
+    @GetMapping(path = {"/{id}"})
+    public Seance findSeanceById(@PathVariable int id){
+        return seanceService.findBySeanceId(id);
+    }
+
+    @PostMapping
+    public void create(@RequestBody Seance seance){
+        seanceService.save(seance);
+    }
+
+    @PutMapping(value="/{id}")
+    public void update(@PathVariable("id") int id,
+                       @RequestBody Seance seance){
+        Seance seance_temp=seanceService.findBySeanceId(id);
+        seance_temp.setMovie(seance.getMovie());
+        seance_temp.setSeats(seance.getSeats());
+        seanceService.update(seance_temp);
+    }
+
+}
